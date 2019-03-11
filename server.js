978bcc764fa7cb3ba0cf5461e8b3fe70a174748f
Daniel Castillo <daniel.castillo.marfull.contractor@bbva.com>
2019-03-11 11:53:16 +0100

    schemas

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const { ApolloServer, gql } = require('apollo-server')
 const mongoose = require('mongoose')
 require('dotenv').config({ path: 'variables.env' })
 
+const User = require('./models/User')
+const Post = require('./models/Post')
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('DB connected'))
@@ -21,9 +24,13 @@ const typeDefs = gql`
 `
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  context: {
+    User,
+    Post
+  }
 })
 
 server.listen(4000).then(({ url }) => {
   console.log(`Server listens on ${ url }`)
-})
+}) 
